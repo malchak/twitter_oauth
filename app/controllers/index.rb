776e6b5ea1a69c5338c866743166fa8994e52fec
stephenitis <nguyenhstephen@gmail.com>
2013-05-07 12:32:50 -0700

    store user twitter info in database

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,6 +5,8 @@ end
 get '/sign_in' do
   # the `request_token` method is defined in `app/helpers/oauth.rb`
   redirect request_token.authorize_url
+  # authorize_url is a method on the OAuth::Consumer object in helpers
+
 end
 
 get '/sign_out' do
@@ -15,9 +17,10 @@ end
 get '/auth' do
   # the `request_token` method is defined in `app/helpers/oauth.rb`
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
+
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
-
+  @user = User.create(:username => @access_token.params[:screen_name], :oauth_secret => @access_token.secret, :oauth_token => @access_token.token)
   # at this point in the code is where you'll need to create your user account and store the access token
 
   erb :index
